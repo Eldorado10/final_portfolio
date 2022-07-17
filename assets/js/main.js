619305b14d467b16324bd1c6c40c1cd27385f36f
Eldorado10 <dipshahriarislam.com>
2022-07-18 04:36:03 +0600

    skills added

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,23 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== ACCORDION SKILLS ====================*/
+const skillscontent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
 
+
+function toggleskills(){
+    let itemClass=this.parentNode.className
+
+    for(i=0; i < skillscontent.length; i++){
+        skillscontent[i].className='skills__content skills__close'
+    }
+    if(itemClass==='skills__content skills__close'){
+        this.parentNode.className='skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click',toggleskills)
+})
 
 /*==================== QUALIFICATION TABS ====================*/
 
